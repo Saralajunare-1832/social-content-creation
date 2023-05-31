92f37f264bdc340cb75e9371dd6262014d31cdf3
Saralajunare-1832 <saralajunare93@gmail.com>
2023-05-31 12:47:00 +0530

    "just for practice12"

---
+++ src/test/java/com/contentCreation/ContentManagerTest.java
@@ -0,0 +1,14 @@
+package com.contentCreation;
+
+import lombok.ToString;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ContentManagerTest {
+    @Test
+    public void check(){
+        System.out.println("this to check");
+        System.out.println("this to check");
+    }
+}
